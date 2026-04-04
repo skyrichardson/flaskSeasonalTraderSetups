e50bce55adf60ac51034fe4dd8a93290bf14a09f
skyrichardson <86213662+skyrichardson@users.noreply.github.com>
2026-04-04 20:58:28 +1100

    init

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, url_for, redirect
+import csv
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # now = datetime.now()
+    return redirect(url_for('hello_world', year=2026, month=4))
+@app.route('/<int:year>/<int:month>')
+def hello_world(year, month):
+    period = f'{year}_{month:02d}'  # formats month as 2 digits e.g. 04
+    trades = request.args.get('trades', '10')
+    rr = request.args.get('rr', '0.1')
+    entry_date = request.args.get('entry_date', '')
+    column_header = ['Ticker', 'Win %', 'Avg Win %', 'Avg Loss %',
+                     'Trades', 'Entry', 'Exit', 'Stop', 'P/L Ratio', 'ID']
+    try:
+        with open(f'data/{period}_long_mature_setups.csv', 'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            data = [row for row in data if int(row[20]) >= int(trades)]
+            data = [row for row in data if float(row[10]) >= (float(row[11]) * float(rr))]
+            if entry_date:
+                data = [row for row in data if row[3] == entry_date]
+            # row[10]|float > (row[11]|float * 2)
+
+
+    except FileNotFoundError:
+        data = []
+    return render_template('index.html', data=data,
+                           period=period, header=column_header, trades=trades,
+                           rr=rr, entry_date=entry_date)
+
+
+if __name__ == '__main__':
+    app.run()
